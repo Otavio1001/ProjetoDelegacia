511de544294319907eb1c2d8d5d322cad2ad1a7b
Otavio1001 <50302656+Otavio1001@users.noreply.github.com>
2019-05-30 18:21:15 -0300

    Update ScriptProj.sql

--- ScriptProj.sql
+++ ScriptProj.sql
@@ -1,8 +1,8 @@
-/*CREATE DATABASE "RPDMS"
+CREATE DATABASE "RPDMS"
     WITH 
     OWNER = rpdadm
     ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;*/
+    CONNECTION LIMIT = -1;
 	
 CREATE SEQUENCE policial_ID
 INCREMENT 1
@@ -120,3 +120,88 @@ CREATE TABLE Ocorrencia (
 	UNIQUE (codOcorrencia)
 );
 
+
+
+
+/* CRUD - Insert */
+
+
+INSERT INTO Policial VALUES ('10330','141128441-12','Marvin Branagh','24/09/1963','Afro-Descendente','M','A','Casado','Rua Southern 12 Downtown','2500','ativo','R.P.D.','tenente','Academia de Policia de Raccoon City','Estrategísta');
+INSERT INTO Policial VALUES ('10340','142233440-11','Chris Redfield','14/06/1970','Caucasiano','M','A','Solteiro','Rua Lightning 12 Downtown','2100','ativo','S.T.A.R.S.','oficial','Academia de Policia de Raccoon City','Liderança');
+INSERT INTO Policial VALUES ('10350','143174443-10','Leon S. Kennedy','20/10/1977','Caucasiano','M','B','Solteiro',NULL,'2000','ativo','R.P.D.','oficial','Academia de Policia de Raccoon City','Experiência em armas e artes marciais');
+
+INSERT INTO Civil VALUES ('2001123','132125441-10','Robert Kendo','13/05/1966','Caucasiano','M','O','Casado','Rua Northwest 46 Downtown','empregado','0');
+INSERT INTO Civil VALUES ('2001124','133126442-11','Ben Bertolucci','22/02/1971','Caucasiano',M','AB','Solteiro','Rua ChainHill 76 Downtown','empregado','0');
+INSERT INTO Civil VALUES ('2001125','134128444-14','Seu Madruga','15/06/1954','Caucasiano','M','B','Solteiro','Rua Gentalha 72 Vila','desempregado','0');
+INSERT INTO Civil VALUES ('2001132','136132443-12','Billy Coen','12/07/1979','Caucasiano','M','A','Solteiro',NULL,'empregado','0');
+
+INSERT INTO Suspeito VALUES ('2001132','assassinatos em série',NULL);
+
+INSERT INTO Veiculo VALUES ('50020','GF2223','Ford Crown Victoria','1995','2','disponivel');
+INSERT INTO Veiculo VALUES ('50021','SA8844','Chevrolet Step Van','1986','1','disponivel');
+INSERT INTO Veiculo VALUES ('50022','HJ9771','Chevrolet Caprice','1985','3','disponivel');
+
+INSERT INTO Ocorrencia VALUES ('304034','Suspeito de assassinato em série visto nos arredores das montanhas Arklay, supostamente caucasiano','10340','2001132','2001124','Alta','4001002');
+
+INSERT INTO Caso VALUES ('4001002','pendente');
+
+
+
+/* CRUD - Select */
+
+
+
+SELECT * FROM Policial;
+SELECT * FROM Policial WHERE divisao = 'R.P.D.';
+
+SELECT nome FROM Civil;
+SELECT * FROM Civil WHERE condicao = 'desempregado';
+
+SELECT crime FROM Suspeito;
+
+SELECT veiculoID FROM Veiculo;
+SELECT * FROM Veiculo WHERE ano >= 1990;
+
+SELECT codOcorrencia FROM Ocorrencia;
+
+SELECT condicao FROM Caso;
+
+
+
+/* CRUD - Update */
+
+
+
+UPDATE Policial SET condicao = 'dispensado' WHERE policialID = '10340';
+
+UPDATE Civil SET condicao = 'desempregado' WHERE civilID = '2001124';
+
+UPDATE Civil SET antecedentes = '1' WHERE civilID = '2001132';
+
+UPDATE Suspeito SET cela = '01' WHERE suspeitoID = '2001132';
+
+UPDATE Veiculo SET condicao = 'em uso' WHERE placa = 'GF2223';
+
+UPDATE Ocorrencia SET 
+descricao = 'Suspeito de assassinato em série visto nos arredores das montanhas Arklay, caucasiano, cabelos pretos, regata branca e calças camufladas'
+WHERE codOcorrencia = '304034';
+
+UPDATE Caso SET condicao = 'arquivado' WHERE codCaso = '4001002';
+
+
+
+/* CRUD - Delete */
+
+
+
+DELETE FROM Policial WHERE policialID = '10340';
+
+DELETE FROM Civil WHERE civilID = '2001125';
+
+DELETE FROM Suspeito;
+
+DELETE FROM Veiculo WHERE placa = 'HJ9771';
+
+DELETE FROM Caso;
+
+DELETE FROM Ocorrencia;
